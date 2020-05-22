58d4582693f5e55a12920c07348d00d34de28667
Justin Russo <j.russo@outlook.com>
2020-05-22 03:27:54 -0400

    feat: add starter code

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { Layout } from "../components/Layout";
+
+const IndexPage = () => (
+  <Layout title="Home | Justin Russo">
+    <h1>Hello!</h1>
+  </Layout>
+);
+
+export default IndexPage;
