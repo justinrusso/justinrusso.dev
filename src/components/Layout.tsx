523584fd522df6a42b06b55d09cdc5f04c1b4aba
Justin Russo <j.russo@outlook.com>
2020-05-22 14:43:03 -0400

    feat: initial website styling

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,27 +10,47 @@ export const Layout: React.FunctionComponent<Props> = ({
   children,
   title = "Justin Russo",
 }) => (
-  <div>
+  <>
     <Head>
       <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{" "}
-        |{" "}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{" "}
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
+    <div className="flex flex-col h-full">
+      <header className="flex items-center justify-between flex-wrap p-6 lg:hidden">
+        <div className="flex items-center flex-shrink-0 mr-6">
+          <span className="font-semibold text-xl tracking-tight">
+            Justin Russo
+          </span>
+        </div>
+        <div className="bloc">
+          <button className="flex items-center px-3 py-2 border rounded">
+            <svg
+              className="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
+      </header>
+      <div className="flex flex-1">
+        <nav className="hidden lg:block w-2/12 flex flex-row">
+          <div className="font-semibold text-xl tracking-tight">
+            Justin Russo
+          </div>
+          <Link href="/">
+            <a>Home</a>
+          </Link>{" "}
+          |{" "}
+          <Link href="/about">
+            <a>About</a>
+          </Link>{" "}
+        </nav>
+        <div className="flex-1">
+          <div className="lg:mx-10 lg:mt-2">{children}</div>
+        </div>
+      </div>
+    </div>
+  </>
 );
---
+++ src/pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <style>
+            {`
+            #__next { height: 100% }
+          `}
+          </style>
+        </Head>
+        <body className="bg-gray-100 dark:bg-gray-900 h-screen">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
