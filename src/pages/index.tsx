f9d47e3ed35f2e1e88b2f81fd1c4770da881944a
Justin Russo <j.russo@outlook.com>
2020-05-25 01:43:42 -0400

    fix: layout potentially re-rendered on page changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
-import { Layout } from "../components/Layout";
-
 const IndexPage = () => (
-  <Layout title="Home | Justin Russo">
+  <>
     <h1>Hello!</h1>
-  </Layout>
+  </>
 );
 
 export default IndexPage;
