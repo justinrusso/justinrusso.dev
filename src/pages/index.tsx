cf6e1114e6591188d0b15d2c04b57e9dddb35a5f
Justin Russo <j.russo@outlook.com>
2020-05-25 02:31:11 -0400

    feat: add index card and animate on entry

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
+import { motion } from "framer-motion";
+
 const IndexPage = () => (
-  <>
-    <h1>Hello!</h1>
-  </>
+  <motion.div
+    className="page-card-wrapper"
+    initial={{ y: "100%" }}
+    animate={{ y: "0%" }}
+    exit={{ y: "-100%" }}
+    transition={{ duration: 2 }}
+  >
+    <div className="lg:rounded-t-lg overflow-hidden shadow-lg min-w-full min-h-screen dark:bg-gray-900">
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">Home</div>
+        <p className="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+          nihil.
+        </p>
+      </div>
+    </div>
+  </motion.div>
 );
 
 export default IndexPage;
