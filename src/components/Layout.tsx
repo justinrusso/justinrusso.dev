32a3685501d48e634c4c38e7c24e1746c047ee39
Justin Russo <j.russo@outlook.com>
2022-01-02 14:44:10 -0500

    refactor: styled component for main wrapper

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,11 +2,24 @@ import "aos/dist/aos.css";
 
 import { FC, useEffect } from "react";
 import AOS from "aos";
+import styled from "styled-components";
 
 import Footer from "./Footer";
 import Head from "./Head";
 import Nav from "./Nav";
 import GlobalStyle from "../theme/GlobalStyle";
+
+const Main = styled.main`
+  width: 100%;
+  height: auto;
+  clear: both;
+  float: left;
+  padding-left: 320px;
+  transition: all 0.5s ease;
+  @media screen and (min-width: 1200px) and (max-width: 1500px) {
+    padding-left: 288px;
+  }
+`;
 
 const Layout: FC = ({ children }) => {
   useEffect(() => {
@@ -17,7 +30,7 @@ const Layout: FC = ({ children }) => {
       <Head />
       <GlobalStyle />
       <Nav />
-      <main className="edina_tm_mainpart">{children}</main>
+      <Main>{children}</Main>
       <Footer />
     </>
   );
