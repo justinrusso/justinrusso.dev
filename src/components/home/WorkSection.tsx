c217030687d98bf73413bf903d80276dd0e5fb21
Justin Russo <j.russo@outlook.com>
2022-01-03 22:21:09 -0500

    feat: add Slurp project

--- src/components/home/WorkSection.tsx
+++ src/components/home/WorkSection.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 
 import Container from "../common/Container";
+import Project from "../Project";
 import SectionHeader from "../SectionHeader";
 
 const WorkSection: FC = () => {
@@ -8,6 +9,16 @@ const WorkSection: FC = () => {
     <div id="work">
       <Container>
         <SectionHeader>Some of My Work</SectionHeader>
+        <Project
+          name="Slurp"
+          url="https://jrusso-slurp.herokuapp.com/"
+          imageSrc="https://github.com/justinrusso/Slurp/blob/main/docs/images/homepage_ss.jpg?raw=true"
+          repoUrl="https://github.com/justinrusso/Slurp"
+          tech={["JavaScript", "Express", "PostgreSQL", "React", "Redux"]}
+        >
+          Slurp is the perfect way to find the best Ramen in your area and is
+          inspired by Yelp.
+        </Project>
       </Container>
     </div>
   );
