07a7a9b96b6270aaef52720283b97f8a4765cbef
Justin Russo <j.russo@outlook.com>
2022-01-04 17:21:49 -0500

    feat: AnimateInView

---
+++ src/components/utils/AnimateInView.tsx
@@ -0,0 +1,55 @@
+import { CSSTransition } from "react-transition-group";
+import {
+  createElement,
+  CSSProperties,
+  ElementType,
+  FC,
+  PropsWithChildren,
+} from "react";
+import { useInView } from "react-intersection-observer";
+import type { CSSTransitionProps } from "react-transition-group/CSSTransition";
+
+type AnimateInViewProps = {
+  /**
+   * Render the wrapping element as this element.
+   * @default `'div'`
+   */
+  as?: ElementType<any>;
+  className?: string;
+  style?: CSSProperties;
+  transitionProps?: Omit<CSSTransitionProps, "in">;
+};
+
+const AnimateInView: FC<PropsWithChildren<AnimateInViewProps>> = ({
+  as,
+  children,
+  className,
+  style,
+  transitionProps,
+}) => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+  });
+
+  const transitionWrapperClasses =
+    transitionProps?.classNames &&
+    `${transitionProps?.classNames} ${transitionProps?.classNames}-enter`;
+
+  const wrapperClasses = `${transitionWrapperClasses || ""} ${className}`;
+
+  return (
+    <CSSTransition in={inView} timeout={0} {...transitionProps}>
+      {createElement(
+        as || "div",
+        {
+          ref: ref,
+          className: wrapperClasses,
+          style,
+        },
+        children
+      )}
+    </CSSTransition>
+  );
+};
+
+export default AnimateInView;
